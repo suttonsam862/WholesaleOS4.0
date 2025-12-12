6cbb6038f39cbf342e4a07100f46abd007217704
samsutton <41090967-samsutton@users.noreply.replit.com>
2025-12-12 09:20:05 +0000

    Restored to '6a1c0fb518cf639c02272493d6822111bea5ef23'
    Replit-Restored-To: 6a1c0fb518cf639c02272493d6822111bea5ef23

--- client/src/components/views/index.ts
+++
@@ -1,7 +0,0 @@
-export { LensHeader } from "./LensHeader";
-export { CardGrid, StatCard, ProgressRing } from "./CardGrid";
-export { KanbanBoard, KanbanCard, type KanbanColumn } from "./KanbanBoard";
-export { Timeline, TimelineItemCard } from "./Timeline";
-export { SplitView } from "./SplitView";
-export { CountdownCard, CountdownGrid } from "./CountdownCards";
-export { GanttView } from "./GanttView";
